fa11482ac00b6d045f2a45471d81c4af4c0f06bc
Roshan <roshanshrestha0921@gmail.com>
2024-04-01 19:06:36 +0545

    qr code generator complete

---
+++ QR_Code_Generator.py
@@ -0,0 +1,18 @@
+import qrcode
+
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black",back_color="white")
+    img.save("E:/python/QR_Image.png")
+
+text=input("Enter text or link: ")
+generate_qrcode(text)
